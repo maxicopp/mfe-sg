3e1e4a4f519ab5ce25e4d0473957e61cc6f8d8fe
Maximiliano Cóppola <maxicopp@outlook.com>
2022-11-16 17:42:40 -0300

    Running memory history in isolation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -8,7 +8,9 @@ const mount = (el, { onNavigate }) => {
 
     const history = createMemoryHistory();
 
-    history.listen(onNavigate);
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
 
     ReactDOM.render(
         <App history={history} />,
@@ -22,7 +24,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
